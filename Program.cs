fa82d8f8f3af903fcffc2d33436afe22ff6c6e72
Vishwanatha Hosathota <Vishwanatha.Hosathota@ibm.com>
2025-04-30 13:25:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿//https://community.ibm.com/community/user/blogs/tapan-kumar-ghosh/2023/09/11/deploy-and-capture-db2trc-on-red-hat-openshift
+using IBM.Data.Db2;
+
+namespace OCTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Using DB2 .NET6 provider");
+
+            string uid = Environment.GetEnvironmentVariable("uid");
+
+            string pwd = Environment.GetEnvironmentVariable("pwd");
+
+            string server = Environment.GetEnvironmentVariable("server");
+
+            string db = Environment.GetEnvironmentVariable("db");
+
+            string security = Environment.GetEnvironmentVariable("security");
+
+            //Connection String
+
+            string connString = "uid=" + uid + ";pwd=" + pwd + ";server=" + server + ";database=" + db + ";Security=" + security;
+
+            DB2Connection con = new DB2Connection(connString);
+
+            con.Open();
+
+            Console.WriteLine("Connection Opened successfully");
+
+            con.Close();
+
+            Console.WriteLine("Connection Closed");
+
+        }
+    }
+}
+
+
+
